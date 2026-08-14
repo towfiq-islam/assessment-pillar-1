3853e90d75413b6acfc2e95b433854981e9080e2
towfiq-islam <tofiqislam03@gmail.com>
2026-08-14 10:08:33 +0600

    chore: initializing Next.js project with TypeScript and Tailwind CSS

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "JCREA",
+  description: "An E-commerce Website",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${urbanist.variable} antialiased`}>
+      <body>{children}</body>
+    </html>
+  );
+}
